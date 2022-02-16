382a3ef6b4076c87e58669b98f7c6e90ae5126c5
Tsaikkari <kirsi.trospe@gmail.com>
2022-02-16 21:58:43 +0100

    patch

---
+++ src/models/Weight.ts
@@ -0,0 +1,37 @@
+import { Schema, Document, model } from 'mongoose'
+
+export type WeightDocument = Document & {
+  currentWeight: Number
+  goalWeight: Number
+  achievedWeight: Number
+  user: Schema.Types.ObjectId
+  week: Schema.Types.ObjectId
+}
+
+const weightSchema = new Schema(
+  {
+    currentWeight: {
+      type: Number,
+      default: 0,
+    },
+    goalWeight: {
+      type: Number,
+      default: 0,
+    },
+    achievedWeight: {
+      type: Number,
+      default: 0
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    week: { type: Schema.Types.ObjectId, ref: 'Week' },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+export default model<WeightDocument>('Weight', weightSchema)
--- src/routers/week.ts
+++ src/routers/week.ts
@@ -14,7 +14,7 @@ const router = express.Router()
 router.get('/', verifyToken, findAll)
 router.get('/user', verifyToken, findUserWeeks)
 router.post('/', verifyToken, createWeek)
-router.put('/:weekId', verifyToken, updateWeek)
+router.patch('/:weekId', verifyToken, updateWeek)
 router.delete('/:weekId', verifyToken, deleteWeek)
 
 export default router
